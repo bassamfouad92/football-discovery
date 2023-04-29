fa2ad4e9ba844519834d5f25cf73863e395260fc
Bassam <bassam.fouad@10pearls.com>
2023-04-29 21:27:11 +0500

    Initial Commit

---
+++ FootballDiscovery/FootballDiscoveryApp.swift
@@ -0,0 +1,12 @@
+//  Created on 29/04/2023.
+
+import SwiftUI
+
+@main
+struct FootballDiscoveryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
